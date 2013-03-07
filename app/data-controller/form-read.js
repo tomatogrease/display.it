0a39b0a5597174097cce5386f6c11381f0bd0482
tomatogrease <rozdelacruz@gmail.com>
2013-03-06 22:02:49 -0800

    Read form values and save it into an array

--- app/data-controller/form-read.js
+++ app/data-controller/form-read.js
@@ -1,20 +1,35 @@
 var formReader = (function(){
 
     function readForm(formID){
-        var elements = formID.children('input');
-        for (var i = elements.length - 1; i >= 0; i--) {
-            console.log($(elements[i]).attr("id");       
-        };
+        
+        var elements = formID.children('input').not('input[type="submit"]'),
+            eleLength = elements.length,
+            valueArray = new Array();
+
+        for (var i = eleLength - 1; i >= 0; i--) {
+            var ele = $(elements[i]);
+            valueArray[ele.attr('id')] = ele.val();
+        }
+
+        return valueArray;
     }
-    
+
+    function submitListen(formID){
+        $(formID).children('input[type="submit"]').on("click",function(){
+            console.log(readForm(formID));
+            return false;
+        });
+    }
+
     return {
         init : function(formID){
-            readForm(formID);
+            submitListen(formID);
         }
     }
 
 })();
 
 $(document).ready(function(){
-    formReader.init($('#recipeForm'),'input');
+    formReader.init($('#recipeForm'));
 });
+
